52f42f79367d03c4526c081b885beb113f574760
장성민 <aristatait@gmail.com>
2022-10-25 10:03:40 +0900

    feat: painting line

--- app.js
+++ app.js
@@ -1,24 +1,52 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
-
-// canvas 의 크기를 정한다
 canvas.width = 800;
 canvas.height = 800;
 
-// 팔
-context.fillRect(210 - 40, 200 - 40, 15, 100);
-context.fillRect(350 - 40, 200 - 40, 15, 100);
+// 라인의 굵기
+context.lineWidth = 2;
 
-// 몸
-context.fillRect(260 - 40, 200 - 40, 60, 200);
+// 컬러
+const colors = [
+  "#f03e3e",
+  "#e64980",
+  "#be4bdb",
+  "#7950f2",
+  "#4c6ef5",
+  "#228be6",
+  "#15aabf",
+  "#12b886",
+  "#40c057",
+  "#82c91e",
+  "#fab005",
+  "#fd7e14"
+];
 
-// 머리
-context.arc(250, 100, 50, 0, 2 * Math.PI);
-context.fill();
+let moveToX = 0;
+let moveToY = 0;
 
-// 눈
-context.beginPath();
-context.fillStyle = "red";
-context.arc(270, 80, 8, 1 * Math.PI, 2 * Math.PI);
-context.arc(230, 80, 8, 1 * Math.PI, 2 * Math.PI);
-context.fill();
+function getRandomLoc() {
+  return Math.floor(Math.random() * 800);
+}
+
+function moveToClickPoint(e) {
+  moveToX = getRandomLoc();
+  moveToY = getRandomLoc();
+}
+
+canvas.addEventListener("click", moveToClickPoint);
+
+function drawLineToMousePoint(e) {
+  context.beginPath();
+
+  context.moveTo(moveToX, moveToY);
+
+  const randomNumber = Math.floor(Math.random() * colors.length);
+  const color = colors[randomNumber];
+  context.strokeStyle = color;
+
+  context.lineTo(e.offsetX, e.offsetY);
+  context.stroke();
+}
+
+canvas.addEventListener("mousemove", drawLineToMousePoint);
