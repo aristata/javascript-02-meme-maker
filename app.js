4681940e985eadd2fd3e0e2324da56ca0587c8c8
장성민 <aristatait@gmail.com>
2022-11-03 16:46:10 +0900

    feat: 2.5 filling mode

--- app.js
+++ app.js
@@ -1,14 +1,19 @@
+// dom selector ----------------------------------------------------------------
 const canvas = document.querySelector("canvas");
 const lineWidthInput = document.getElementById("line-width-input");
 const colorInput = document.querySelector("#color-input");
-
-// color option 들을 선택한다
 const colorOptions = document.querySelectorAll(".color-options");
+const changeModeButton = document.querySelector("#changeModeButton");
+const currentModeSpan = document.querySelector("#currentModeSpan");
 
 const context = canvas.getContext("2d");
 context.lineWidth = lineWidthInput.value;
 
+// grobal variable -------------------------------------------------------------
 let isPainting = false;
+let isFilling = false;
+
+// functions -------------------------------------------------------------------
 function onMoveMouse(event) {
   if (isPainting) {
     context.lineTo(event.offsetX, event.offsetY);
@@ -31,7 +36,6 @@ function onChangeLineWidth(event) {
   context.lineWidth = event.target.value;
 }
 
-// 색깔을 바꾸는 함수
 function changeColor(newColor) {
   context.strokeStyle = newColor;
   context.fillStyle = newColor;
@@ -41,20 +45,43 @@ function onChangeColor(event) {
   changeColor(event.target.value);
 }
 
-// color option 선택 함수
 function onClickColorOption(event) {
   changeColor(event.target.dataset.color);
   colorInput.value = event.target.dataset.color;
 }
 
+// 모드 변경 함수
+function onClickChangeModeButton() {
+  if (isFilling) {
+    // 채우기 모드인 경우 그리기 모드로 변경한다
+    isFilling = false;
+    currentModeSpan.innerText = '현재 모드는 "그리기" 모드';
+  } else {
+    // 그리기 모드인 경우 채우기 모드로 변경한다
+    isFilling = true;
+    currentModeSpan.innerText = '현재 모드는 "채우기" 모드';
+  }
+}
+
+function onClickCanvas() {
+  if (isFilling) {
+    context.fillRect(0, 0, 800, 800);
+  }
+}
+
+// event listener --------------------------------------------------------------
 canvas.addEventListener("mousemove", onMoveMouse);
 canvas.addEventListener("mousedown", onStartPainting);
 canvas.addEventListener("mouseup", onCancelPainting);
 canvas.addEventListener("mouseleave", onCancelPainting);
 lineWidthInput.addEventListener("change", onChangeLineWidth);
 colorInput.addEventListener("change", onChangeColor);
-
-// color options 에 이벤트를 추가한다
 Array.from(colorOptions).forEach((colorOption) =>
   colorOption.addEventListener("click", onClickColorOption)
 );
+
+// 버튼에 이벤트 걸기
+changeModeButton.addEventListener("click", onClickChangeModeButton);
+
+// 캔버스에 이벤트 걸기
+canvas.addEventListener("click", onClickCanvas);
